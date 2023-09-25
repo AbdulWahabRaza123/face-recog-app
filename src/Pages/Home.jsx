d09fd1875a344012b00564e4dd91f01049894be6
AbdulWahabRaza123 <abdulwahabraza123@gmail.com>
2023-09-25 21:32:16 +0500

    live web cam added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -28,9 +28,26 @@ const Home = (props) => {
   const webcamRef = useRef(null);
 
   const [capturing, setCapturing] = useState(false);
-  const capture = useCallback(() => {
+  function base64StringtoFile(base64String, filename) {
+    const arr = base64String.split(",");
+    const mime = arr[0].match(/:(.*?);/)[1];
+    const bstr = atob(arr[1]);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+
+    return new File([u8arr], filename, { type: mime });
+  }
+  const capture = useCallback(async () => {
     const pictureSrc = webcamRef.current.getScreenshot();
     setPicture(pictureSrc);
+    const mkFile = base64StringtoFile(pictureSrc, "image.jpg");
+    setTranslation("Loading...");
+    await UploadData(mkFile);
+    // console.log("This is picture ", mkFile);
   });
   const UploadData = async (data) => {
     try {
